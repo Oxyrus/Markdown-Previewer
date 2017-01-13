2ca9136abba92f03e1dc06086ae7f70ee8de83fb
Andrés Pérez <andresxz09@gmail.com>
2017-01-13 15:12:34 -0500

    Finish application

--- src/components/App.js
+++ src/components/App.js
@@ -16,16 +16,25 @@ export default class App extends Component {
     });
   }
 
+  componentWillMount() {
+    this.setState({
+      text: '# Hey there'
+    })
+  }
+
   render() {
     let md = new Remarkable();
     let html = md.render(this.state.text);
     return (
+      <div>
       <div className="container">
         <h1>Markdown Previewer</h1>
-        <textarea onChange={e => this.setText(e)} ref="text"># You can write here :)</textarea>
+        <textarea onChange={e => this.setText(e)} ref="text" value={this.state.text}></textarea>
         <div className="preview">
           <div dangerouslySetInnerHTML={{__html: html}} className="inner-preview" />
         </div>
+      </div>
+        <footer>Crafted with ♥ by <a href="https://github.com/Oxyrus" target="_blank">Andrés Pérez</a></footer>
       </div>
     );
   }
